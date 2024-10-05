e12f9b14c53193b3cf3e0119e816c05ac39ff102
Jace Galloway <jace.galloway@gmail.com>
2024-10-05 14:57:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Professional README Generator
+
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Description
+This application dynamically generates a professional README file based on user input, using Node.js and Inquirer. It helps developers quickly create a high-quality README for their GitHub projects.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- [Credits](#credits)
+[License](#license)
+
+## Installation
+First Clone the repository THEN Run 'npm install' to install dependencies THEN Run 'node index.js' to start the application
+
+## Usage
+To use this application, simply run 'node index.js' and answer the prompts. A README file will be generated based on the provided input.
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+Contributions are welcome. Please submit a pull request or open an issue for any feature requests or bugs.
+
+## Tests
+To run tests, use the following command: npm test
+
+## Questions
+For any questions, please contact me at jace.galloway@gmail.com. You can also find my GitHub profile at [JaceG](https://github.com/JaceG).
+
+## Credits
+ Credits: This project was created as part of a coding bootcamp challenge. - Sources and References: Portions of the code and guidance were provided with assistance from ChatGPT, an AI language model developed by OpenAI.
+
+ ## Screenrecording Video
+https://drive.google.com/drive/folders/14SxIFK5OkULclpT9rS5iMv6qplleJJgT?usp=sharing
---
+++ index.js
@@ -0,0 +1,77 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide the installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'List any credits or references for this project (e.g., ChatGPT, instructors, etc.):',
+  }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Successfully created README.md!')
+  );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    const markdown = generateMarkdown(responses);
+    writeToFile('README.md', markdown);
+  });
+}
+
+// Function call to initialize app
+init();
